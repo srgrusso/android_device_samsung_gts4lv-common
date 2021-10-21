1e02c5e81b24e511446b848323bf398c10e533fb
Jan Altensen <info@stricted.net>
2021-10-20 21:13:56 -0500

    gts4lv-common: add RemovePackages
    just set LOCAL_SRC_FILES to /dev/null and it works again

--- gts4lv.mk
+++ gts4lv.mk
@@ -260,6 +260,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
