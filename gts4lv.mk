d5425b4eb01599435572c33a7ba9e39ccb584537
srgrusso <srgrusso240@gmail.com>
2023-02-25 12:50:19 -0600

    gts4lv-common: Build Aperture

--- gts4lv.mk
+++ gts4lv.mk
@@ -100,6 +100,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5-service \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    Aperture
+
 # Charger
 PRODUCT_PACKAGES += \
     libsuspend
